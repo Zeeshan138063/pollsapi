036094a30f05ce4af9b5ad42ffdac971719a248b
Zeeshan <m.zoneclout@gmail.com>
2019-03-12 19:25:12 +0500

    Using coreapi for documentation

--- pollsapiprac/urls.py
+++ pollsapiprac/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
 
 schema_view = get_swagger_view(title='Polls API')
 
@@ -26,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
     path('swagger-docs/', schema_view),
+    path('docs/', include_docs_urls(title='Polls API')),
 ]
