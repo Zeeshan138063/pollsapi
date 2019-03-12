bb983bc1c4ce92da6afaf33cf08fcb3479339183
Zeeshan <m.zoneclout@gmail.com>
2019-03-12 17:21:28 +0500

    added GENERIC VIEWS

--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,45 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
+#
+# from .models import Poll, Choice
+# from  .serializers import PollSerializer
+#
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
+#
+#
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
+
+
+
+'''
+The generic views of Django Rest Framework help us in code reusablity. 
+They infer the response format and allowed methods from the serializer class and base class.
+
+'''
+from rest_framework import generics
 
 from .models import Poll, Choice
-from  .serializers import PollSerializer
-
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+from .serializers import PollSerializer, ChoiceSerializer,\
+    VoteSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
